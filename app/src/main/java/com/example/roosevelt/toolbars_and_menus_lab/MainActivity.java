c274d3a73306bcd9db23ce1239ded827534fb5da
Jae Anonas <jae.anonas@gmail.com>
2016-07-21 17:18:34 -0400

    Jae Anonas

---
+++ app/src/main/java/com/example/roosevelt/toolbars_and_menus_lab/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.roosevelt.toolbars_and_menus_lab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setCollapsible(true);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Podcast");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.podcast_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
